c499b593dfce50daa83a9205de3287bb64f52643
Tzohar <73159455+Tzohar@users.noreply.github.com>
2026-01-28 14:14:25 +0200

    Update generate_data.py

--- training/generate_data.py
+++ training/generate_data.py
@@ -8,17 +8,13 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.config import Config 
 
-## Initialize Faker for generating fake data (names, addresses, etc.)
 fake = Faker();
 
-# --- CONFIGURATION ---
 NUM_SAMPLES = 5000  # Size of our  "Synthetic Leak"
 OUTPUT_FILE = Config.RAW_DATA_FILE # Output path for the generated data
 
-# --- SECTION 2.3: REUSE PATTERNS (Simulating how people change passwords) ---
 # Common patterns for password reuse - capitalizations and minor substitutions & additions
 def transform_password(base_pw):
-    ## Takes a base password and applies a random transformation to simulate reuse
     transformations = [
         lambda x: x.capitalize(),
         lambda x: x + "1",
@@ -41,7 +37,6 @@ def transform_password(base_pw):
         new_pw = func(new_pw)
     return new_pw
 
-# --- SECTION 2.2: PII PATTERNS (Simulating PII usage) ---
 def generate_pii_password(profile):
     ### Creates a password derived purely from Personal Info
 
@@ -73,14 +68,13 @@ def generate_synthetic_data():
             "username": fake.user_name(),
             "email": fake.email(),
         }
-        # 2. Generate a "Sister Password" (Old password from a 'leak')
+
         # Sometimes it's totally random, sometimes related to PII
         if random.random() < 0.5:
             sister_password = fake.password(length=10)
         else:
             sister_password = generate_pii_password(profile)
 
-        # 3. Determine the 'Ground Truth' Target Password
         # Scenario A: User modifies their Sister Password (Section 2.3)
         if random.random() < 0.6:
             target_password = transform_password(sister_password)
@@ -89,9 +83,6 @@ def generate_synthetic_data():
         else:
             target_password = generate_pii_password(profile)
             method = "PII-Based"
-
-        # 4. Format the "Input Prompt" exactly as the Model will see it
-        # We concatenate all info into a structured string.
         
         # Format: "Name: [N], Born: [Y], User: [U], OldPW: [PW] ->"
         pii_dict = {
